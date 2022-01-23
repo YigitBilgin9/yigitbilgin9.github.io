7c302a74febc7296b36f968c92613feea051fabe
Yigit Bilgin <bilgin.yigit96@gmail.com>
2022-01-23 16:55:27 +0300

    testing phpmail

---
+++ test.php
@@ -0,0 +1,17 @@
+<?PHP
+$sender = 'bilgin.yigit96@gmail.com';
+$recipient = 'bilgin.yigit96@gmail.com';
+
+$subject = "php mail test";
+$message = "php test message";
+$headers = 'From:' . $sender;
+
+if (mail($recipient, $subject, $message, $headers))
+{
+    echo "Message accepted";
+}
+else
+{
+    echo "Error: Message not accepted";
+}
+?>
